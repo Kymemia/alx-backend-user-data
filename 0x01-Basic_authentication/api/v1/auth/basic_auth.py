210e9c455b6f560f15a39a60dea5e5b7f193cd94
Kymemia <kimemiachristopherr@gmail.com>
2024-09-04 01:41:41 +0300

    basic - base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 class BasicAuth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -31,3 +32,28 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+            ) -> str:
+        """
+        method definition that returns the decoded value
+        of a Base64 string, base64_authorization_header:
+            *Return None if base64_authorization_header is None
+            *Return None if base64_authorization_header is not a string
+            *Return None if base64_authorization_header
+            is not a valid Base64 - you can use try/except
+            *Otherwise, return the decoded value as UTF8 string
+                - you can use decode('utf-8')
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
