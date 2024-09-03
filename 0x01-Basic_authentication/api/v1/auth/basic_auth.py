810f2ca035e9e51a2c3e554b78d3389a5031a46a
Kymemia <kimemiachristopherr@gmail.com>
2024-09-04 01:32:24 +0300

    basic - base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,25 @@ class BasicAuth(Auth):
     """
     class definition that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+            ) -> str:
+        """
+        method definition that returns the Base64 part
+        of the header for a Basic Authentication:
+            *Return None if authorization_header is None
+            *Return None if authorization_header is not a string
+            *Return None if authorization_header doesn’t start
+            by Basic (with a space at the end)
+            *Otherwise, return the value after Basic (after the space)
+        """
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
