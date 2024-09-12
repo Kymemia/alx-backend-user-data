8c9979ea3fb309650c5f931ea7e2d214d35965b4
Kymemia <kimemiachristopherr@gmail.com>
2024-09-12 10:32:22 +0300

    session instead of sessions route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,7 +53,7 @@ def login():
     return response
 
 
-@app.route("/sessions, methods=["DELETE"])
+@app.route("/sessions", methods=["DELETE"])
 def logout():
     """
     logout function that responds to the DELETE /sessions route
