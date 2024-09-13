9ee712fc6a2290e156be44c9789d594b2ead5aae
Kymemia <kimemiachristopherr@gmail.com>
2024-09-13 13:12:36 +0300

    log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,6 +11,7 @@ Args:
     in the log line (message)
 """
 import re
+import logging
 from typing import List
 
 
@@ -23,3 +24,23 @@ def filter_datum(
     return re.sub(f'({"|".join(fields)})=.*?{separator}',
                   lambda match: f"{match.group(1)}={redaction}{separator}",
                   message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        result = super().format(record)
+        return filter_datum(
+                self.fields, self.REDACTION,
+                result, self.SEPARATOR
+                )
